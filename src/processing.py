5c1e91e4e235db930ec9dcd542d3b6e3768bacd7
stanislav_hashibina <stanikin.d@mail.ru>
2025-06-20 00:04:09 +0300

    code correction according to pep8

---
+++ src/processing.py
@@ -0,0 +1,27 @@
+list_of_dict = [
+    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+]
+
+
+def filter_by_state(list_of_dict: list, state='EXECUTED') -> list:
+    filtered_list = []
+    for dict_item in list_of_dict:
+        if dict_item.get('state') == state:
+            filtered_list.append(dict_item)
+    return filtered_list
+
+
+result = filter_by_state(list_of_dict)
+
+
+print(result)
+
+
+def sort_by_date(list_of_dict, reverse=False):
+    return sorted(list_of_dict, key=lambda x: x['date'], reverse=reverse)
+
+
+print(sort_by_date(list_of_dict))
